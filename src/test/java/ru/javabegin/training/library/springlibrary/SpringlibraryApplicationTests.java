157902d5742fed9584bf882d925a8775800bcc68
Мухин Михаил Юрьевич <mmukhin@tsc.ts>
2021-03-24 14:15:51 +0300

    Initial commit

---
+++ src/test/java/ru/javabegin/training/library/springlibrary/SpringlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javabegin.training.library.springlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
